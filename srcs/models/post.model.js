c24eaa5c927eadecd8d6973f0d073069c3abb055
Layton <jihwan8807@naver.com>
2024-06-21 01:20:42 +0900

    hotfix: 서버 연결 수정및 기존 작업 내용 반영

--- srcs/models/post.model.js
+++ srcs/models/post.model.js
@@ -2,6 +2,11 @@ import mongoose from 'mongoose';
 
 const PostSchema = mongoose.Schema(
   {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      require: true,
+    },
     title: {
       type: String,
       required: [true, '제목을 작성해주세요'],
