162fdbc1c13ec517284eaf1d06722e5a681d7870
Layton <jihwan8807@naver.com>
2024-06-23 04:27:16 +0900

    puppeter 설치후 회원가입 스웨거 테스팅 실패

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ import { specs } from './config/swagger.js';
 import { postRouter } from './srcs/routes/post.route.js';
 import { commentRouter } from './srcs/routes/comment.route.js';
 import { imageRouter } from './srcs/routes/image.route.js';
-
+import { loginRouter } from './srcs/routes/loginRouter.js';
+import { signupRouter } from './srcs/routes/signupRouter.js';
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -31,18 +32,23 @@ app.use(
 );
 
 // routes
+app.use('/api/signup', signupRouter);
+app.use('/api/login', loginRouter);
+
 app.use('/api/posts', postRouter);
 app.use('/api/comments', commentRouter);
 app.use('/api/image', imageRouter);
 
-// app.use((err, req, res, next) => {
-//   res.locals.message = err.message;
-//   // 개발환경이면 에러를 출력하고 아니면 출력하지 않기
-//   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-//   res
-//     .status(errStatus.INTERNAL_SERVER_ERROR || err.data.status)
-//     .send(response(err.data));
-// });
+// 이미지 업로드 테스팅할 때 아래 미들웨어 주석처리할 것 (업로드 실패할 경우 에러 메시지가 이상하게 뜸)
+// 애초에 왜 있는지도 잘 모르겠음 - Layton -
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  // 개발환경이면 에러를 출력하고 아니면 출력하지 않기
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res
+    .status(errStatus.INTERNAL_SERVER_ERROR || err.data.status)
+    .send(response(err.data));
+});
 
 const connectDB = async () => {
   try {
